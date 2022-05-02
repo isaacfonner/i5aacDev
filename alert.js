2deed29ba691cf807030af90a368495fcf492326
isaacfonner <61657908+isaacfonner@users.noreply.github.com>
2022-05-02 15:47:31 -0400

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,25 @@
-alert("Hello! I am an alert box!!");
+
+function CodeSlope() {
+
+var password = "BeySqaud";
+var response;
+var entryCount = 0;
+var entryLimit = 4;
+var error = false;
+var correct = false;
+
+while(response != password && !error){
+ if(entryCount < entryLimit){
+ response = window.prompt("Enter Code!"); 
+  entryCount++;
+ } else {
+  error = true;
+   alert("To Many Attemps. Reload to contiune")
+ }
+  
+}
+ if(!error){
+alert("Correct Password")
+   correct = true;
+ }
+}
