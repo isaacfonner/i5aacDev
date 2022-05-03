6e11bf06f49d88bf716323fd8e1165d5417edf21
isaacfonner <61657908+isaacfonner@users.noreply.github.com>
2022-05-03 15:52:09 -0400

    Update alert.js

--- alert.js
+++ alert.js
@@ -8,19 +8,17 @@ var entryLimit = 4;
 var error = false;
 var correct = false;
 
-while(response != password && !error){
+
  if(entryCount < entryLimit){
  response = window.prompt("Enter Code!"); 
-  entryCount++;
- } else {
-  error = true;
-   alert("To Many Attemps. Reload to contiune")
- }
-  
-}
- if(!error){
-alert("Correct Password")
+  if(response = password ) {
+   alert("Correct Password")
   window.location.href("https://mathiasgredal.github.io/Slope-Game/")
    correct = true;
+  } else {
+  
+   alert("Incorrect Password")
+  
+  }
  }
 }
